010b4d5596d33c4bba753693e04488285543edd3
Prasannnnn <prasannamb4362@gmail.com>
2024-10-10 19:20:37 +0530

    notes

--- JavaScript/Session1/print.js
+++ JavaScript/Session1/print.js
@@ -4,3 +4,38 @@
 hello macha
 ennada panra
 */
+
+// var a = 10
+// var b = 20
+// console.log(a+b)
+
+
+// function abc(){
+//      console.log("Hello World")
+// }
+// abc()
+
+// function add(){
+//      var a = 10
+//      var b = 20
+//      c = a+b
+//      console.log(c)
+// }
+// add()
+
+// var factor = "Dhoni"
+// var fplayer = "Dhoni"
+// var fmovie = "Ms Dhoni Untold Story"
+
+// function favorite(){
+//      console.log("favorite actor : "+factor)
+//      console.log("favorite Player : "+fplayer)
+//      console.log("favorite movie : "+fmovie)
+// }
+// favorite()
+
+function na(){
+     return "Prasanna"
+}
+var a = na()
+console.log(a)
